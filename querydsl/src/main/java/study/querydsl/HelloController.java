6d492eb68e96b1bdcf378d0993e246470110c02e
Woong <woongi9@naver.com>
2023-05-14 19:25:11 +0900

    develop) :tada: begin querydsl project

---
+++ querydsl/src/main/java/study/querydsl/HelloController.java
@@ -0,0 +1,25 @@
+package study.querydsl;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * packageName :  study.querydsl
+ * fileName : HelloController
+ * author :  JinWoong
+ * date : 2023/05/14
+ * description :
+ * ===========================================================
+ * DATE                 AUTHOR              NOTE
+ * -----------------------------------------------------------
+ * 2023/05/14           eomjin-ung          init
+ */
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello!";
+    }
+}
