a015bee8cd0debceccc90d20dd851233f5dc55a1
Woong <woongi9@naver.com>
2023-06-18 16:09:02 +0900

    feat) :wrench: add Mybatis and update test code

--- db-2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ db-2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -11,7 +11,8 @@ import org.springframework.context.annotation.Profile;
 
 // @Import(JdbcTemplateV1Config.class)
 // @Import(JdbcTemplateV2Config.class)
-@Import(JdbcTemplateV3Config.class)
+// @Import(JdbcTemplateV3Config.class)
+@Import(MybatisConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
---
+++ db-2/itemservice-db/src/main/java/hello/itemservice/config/MybatisConfig.java
@@ -0,0 +1,44 @@
+package hello.itemservice.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.jdbctemplate.JdbcTemplateItemRepositoryV3;
+import hello.itemservice.repository.mybatis.ItemMapper;
+import hello.itemservice.repository.mybatis.MyBatisItemRepository;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * packageName :  hello.itemservice.config
+ * fileName : JdbcTemplateV1Config
+ * author :  JinWoong
+ * date : 2023/06/17 
+ * description :
+ * ===========================================================
+ * DATE                 AUTHOR              NOTE
+ * -----------------------------------------------------------
+ * 2023/06/17           JinWoong            init
+ */
+
+@Configuration
+@RequiredArgsConstructor
+public class MybatisConfig {
+
+	private final ItemMapper itemMapper;
+
+	@Bean
+	public ItemService itemService() {
+		return new ItemServiceV1(itemRepository());
+	}
+
+	@Bean
+	public ItemRepository itemRepository() {
+		return new MyBatisItemRepository(itemMapper);
+	}
+
+}
