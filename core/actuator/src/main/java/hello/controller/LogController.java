8056cfe996221925184eb94025be89a18a0d994d
Woong <woongi9@naver.com>
2023-10-26 11:05:47 +0900

    feat) :wrench: add actuator

---
+++ core/actuator/src/main/java/hello/controller/LogController.java
@@ -0,0 +1,33 @@
+package hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * packageName :  hello
+ * fileName : LogController
+ * author :  JinWoong
+ * date : 10/26/23 
+ * description :
+ * ===========================================================
+ * DATE                 AUTHOR              NOTE
+ * -----------------------------------------------------------
+ * 10/26/23           JinWoong            init
+ */
+
+@Slf4j
+@RestController
+public class LogController {
+
+	@GetMapping("/log")
+	public String log() {
+		log.trace("trace log");
+		log.debug("debug log");
+		log.info("info log");
+		log.warn("warn log");
+		log.error("error log");
+		return "ok";
+	}
+}
